04fa0d4f1a18464c4d57ceb45f4b82098618522d
longj18 <maxsoccerssport@hotmail.com>
2022-03-24 11:47:18 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from tkinter import *
+from tkinter import ttk
+
+# window = Tk()
+#
+# window.mainloop()
+
+# root = Tk()
+# frm = ttk.Frame(root, padding=20)
+# frm.grid()
+# ttk.Label(frm, text='Hello World!').grid(column=0, row=0)
+# ttk.Button(frm, text="Quit", command=root.destroy).grid(column=0, row=1)
+# root.mainloop()
+
+class App(Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.pack()
+
+        self.entrythingy = Entry()
+        self.entrythingy.pack()
+
+        # Create the application variable.
+        self.contents = StringVar()
+        # Set it to some value.
+        self.contents.set("this is a variable")
+        # Tell the entry widget to watch this variable.
+        self.entrythingy["textvariable"] = self.contents
+
+        # Define a callback for when the user hits return.
+        # It prints the current value of the variable.
+        self.entrythingy.bind('<Key-Return>',
+                             self.print_contents)
+
+    def print_contents(self, event):
+        print("Hi. The current entry content is:",
+              self.contents.get())
+
+root = Tk()
+myapp = App(root)
+myapp.mainloop()
