efaeff54a2c4b7852f6553af1a828031626463c6
longj18 <maxsoccerssport@hotmail.com>
2022-03-25 15:42:51 -0400

    added functionality for clear button

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from tkinter import *
 from tkinter import filedialog as fd
-from PIL import ImageTk, Image
+from tkinter.filedialog import asksaveasfile, SaveAs
+from PIL import ImageTk, Image, ImageFont, ImageDraw, ImageFile
+
 
 window = Tk()
 window.title("Image Watermarking Tool")
 window.config(padx=0, pady=0, bg='#c0c0c0')
 window.geometry('600x600')
-window.maxsize(800, 800)
+window.maxsize(900, 900)
 
 frame = Frame(window, bg='#a0c0c0')
 frame.config()
 frame.place(relx=.5, rely=.5, anchor=CENTER, relheight=1, relwidth=1)
-# canvas = Canvas(frame, height=400, width=500)
+# canvas = Canvas(frame)
 # canvas.place(relx=.5, rely=.5, anchor=CENTER)
 
 # Close Application
@@ -22,30 +24,41 @@ window.config(menu=my_menu)
 
 # functions
 def save():
-    pass
-
+    file = asksaveasfile(mode='w', defaultextension=".png")
+    if file:
+        imgpil = ImageTk.getimage(image)
+        imgpil.save(file)
 
 def add_image():
+    global image_label, image
     path = fd.askopenfilename()
-    # filename
     image = ImageTk.PhotoImage(Image.open(path))
-    my_image = Label(window, image=image)
-    my_image.photo=image
-    my_image.place(relx=.5, rely=.5, anchor=CENTER)
-    # canvas.create_image(100, 100, image=image)
-    # with Image.open(filename) as im:
-    #     canvas.create_image(100, 100, image=im)
+    image_label = Label(frame, image=image)
+    image_label.photo = image
+    image_label.place(relx=.5, rely=.5, anchor=CENTER)
+
 
 def clear_image():
-    print('image cleared')
+    image_label.destroy()
 
 
 def add_text():
-    print('text added')
+    global my_image
+    # Get image
+    image = my_image
+    # Define the font
+    text_font = ImageFont.truetype('arial.ttf', 32)
+    # Get text to add to image
+    text_to_add = text.get()
 
+    # Edit the image
+    edit_image = ImageDraw.Draw(image)
+    edit_image.text((150, 300), text_to_add, ('red'), font=text_font)
 
-def add_logo():
-    print('logo added')
+def add_watermark():
+    path = fd.askopenfilename()
+    watermark = ImageTk.PhotoImage(Image.open(path))
+
 
 
 # Create menu item
@@ -67,7 +80,10 @@ clear_img.place(relx=.6, rely=.9, anchor=CENTER)
 # bottom row of buttons
 add_button = Button(frame, text="Add Text", command=add_text)
 add_button.place(relx=.4, rely=.1,anchor=CENTER)
-add_logo = Button(frame, text="Add Logo", command=add_logo)
+add_logo = Button(frame, text="Add Logo", command=add_watermark)
 add_logo.place(relx=.6, rely=.1, anchor=CENTER)
 
+#text box
+text = Entry(window, font=('Helvetica', 20))
+text.place(relx=.5, rely=.82, anchor=CENTER)
 window.mainloop()
