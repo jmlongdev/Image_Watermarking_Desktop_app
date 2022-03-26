f99e5eff66f5bfb9fd31a2b333b31f891b69959b
longj18 <maxsoccerssport@hotmail.com>
2022-03-26 00:25:17 -0400

    /watermark functionality added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import filedialog as fd
 from tkinter.filedialog import asksaveasfile, SaveAs
-from PIL import ImageTk, Image, ImageFont, ImageDraw,
+from PIL import ImageTk, Image, ImageFont, ImageDraw
 
 
 window = Tk()
@@ -9,32 +9,34 @@ window.title("Image Watermarking Tool")
 window.config(padx=0, pady=0, bg='#c0c0c0')
 window.geometry('600x600')
 window.maxsize(900, 900)
-
+global image_marked, imageTk
 frame = Frame(window, bg='#a0c0c0')
 frame.config()
 frame.place(relx=.5, rely=.5, anchor=CENTER, relheight=1, relwidth=1)
-# canvas = Canvas(frame)
-# canvas.place(relx=.5, rely=.5, anchor=CENTER)
+
 
 # Close Application
 my_menu = Menu(window)
 window.config(menu=my_menu)
 
 
-
 # functions
 def save():
     file = asksaveasfile(mode='wb', defaultextension=".png")
     if file:
-        imgpil = ImageTk.getimage(image)
+        imgpil = ImageTk.getimage(imageTk)
         imgpil.save(file)
 
 def add_image():
-    global image_label, image
+    global image_label, image_tk
+
+    # opens and creates an ImageTk.PhotoImage object
     path = fd.askopenfilename()
-    image = ImageTk.PhotoImage(Image.open(path))
-    image_label = Label(frame, image=image)
-    image_label.photo = image
+    image_tk = ImageTk.PhotoImage(Image.open(path))
+
+    # displays the image
+    image_label = Label(frame, image=image_tk)
+    image_label.photo = image_tk
     image_label.place(relx=.5, rely=.5, anchor=CENTER)
 
 
@@ -43,16 +45,35 @@ def clear_image():
 
 
 def add_text():
-    # Get image
-    image = image
-    # Define the font
-    text_font = ImageFont.truetype('arial.ttf', 32)
-    # Get text to add to image
-    text_to_add = text.get()
+    # Becomes an Image object again
+    watermark_img = ImageTk.getimage(image_tk)
+    width, height = watermark_img.size
+    draw = ImageDraw.Draw(watermark_img)
+    text = "sample watermark"
+    text_font = ImageFont.load_default()
+    textwidth, textheight = draw.textsize(text, text_font)
+    margin = 10
+    x = width - textwidth - margin
+    y = height - textheight - margin
+    draw.text((x, y), text, font=text_font)
 
-    # Edit the image
-    edit_image = ImageDraw.Draw(image)
-    edit_image.text((150, 300), text_to_add, ('red'), font=text_font)
+    ##### image saves with watermark but need to be able to see the watermark first before saving. also,
+    ##### the clear button doesnt clear the watermarked image
+
+    file = asksaveasfile(mode='wb', defaultextension=".png")
+    if file:
+        # imgpil = ImageTk.getimage(imageTk)
+        # imgpil.save(file)
+        watermark_img.save(file)
+
+    image_marked = ImageTk.PhotoImage(watermark_img)
+
+    image_label = Label(frame, image=image_marked)
+    image_label.photo = image_marked
+    image_label.place(relx=.5, rely=.5, anchor=CENTER)
+
+
+    # edit_image.text((150, 300), text_to_add, ('red'), font=text_font)
 
 def add_watermark():
     pass
@@ -69,13 +90,11 @@ file_menu.add_separator()
 file_menu.add_command(label='Close', command=window.quit)
 
 
-
 # top row of buttons
 add_image = Button(frame, text="Add Image", command=add_image)
 add_image.place(relx=.4, rely=.9, anchor=CENTER)
 clear_img = Button(frame, text="Clear", command=clear_image)
 clear_img.place(relx=.6, rely=.9, anchor=CENTER)
-
 
 # bottom row of buttons
 add_button = Button(frame, text="Add Text", command=add_text)
@@ -84,6 +103,6 @@ add_logo = Button(frame, text="Add Logo", command=add_watermark)
 add_logo.place(relx=.6, rely=.1, anchor=CENTER)
 
 #text box
-text = Entry(window, font=('Helvetica', 20))
-text.place(relx=.5, rely=.82, anchor=CENTER)
+text_box = Entry(window, font=('Helvetica', 20))
+text_box.place(relx=.5, rely=.82, anchor=CENTER)
 window.mainloop()
